04ff2af9b901271085dcbe699a84fc3824576d6e
WoojooJeong0v0 <ttottiaa@naver.com>
2024-08-20 17:44:13 +0900

    240820 SQL 문제해결 시험 및 다중 서브쿼리

--- General/Scripts/WORKBOOK1.sql
+++ General/Scripts/WORKBOOK1.sql
@@ -86,3 +86,85 @@ WHERE STUDENT_ADDRESS LIKE '%전주%'
 	
 	-- 2-3 남 교수들 이름과 나이 오름차순 조회
  -- 단 교수 중 2000 이후 출생자 없고 나이는 만 계산
+
+
+
+
+
+-- 3- 1 학생 이름과 주소지를 조회
+-- 출력헤더는 "학생 이름", "주소지"
+-- 이름 오름차순 정렬
+
+ SELECT STUDENT_NAME "학생 이름", STUDENT_ADDRESS "주소지"
+ FROM TB_STUDENT
+ ORDER BY "학생 이름" ASC;
+
+-- 3-2 휴학 중인 학생들 이름과 주민번호를 나이가 적은 순 조회하기
+SELECT STU.STUDENT_NAME "이름", SSN.STUDENT_SSN "주민번호"
+FROM TB_STUDENT STU
+LEFT JOIN TB_STUDENT SSN 
+ON (STU.STUDENT_SSN = SSN.STUDENT_SSN)
+WHERE STU.ABSENCE_YN = 'Y'
+ORDER BY SUBSTR(SSN.STUDENT_SSN, 1, 6) ASC;
+-- *  SELECT에서도 별칭을 적어줘야 정확한 열을 알 수 있음
+-- ** 나이가 적은 순으로 정렬할 때에는 MIN으로 최소값 찾을 필요 없이
+-- 		주민번호 6자리 잘라서 ASC로 정렬해보라고 해도 됨
+--		좀더 정확하게 하고 싶으면 TO_DATE 써서 날짜로 바꿔서 하면 됨
+
+-- 사실 join 안 해도 됨..
+
+-- 3-3 주소지가 강원도나 경기도 학생 중 1900년대 학번을 가진 학생들의
+-- 이름과 학번, 주소를 이름 오름차순으로 조회
+-- 단, 출력헤더에는 "학생이름", "학번", "거주지 주소" 
+SELECT 
+STUDENT_NAME "학생 이름", 
+STUDENT_NO "학번", 
+STUDENT_ADDRESS "거주지 주소"
+FROM TB_STUDENT
+--WHERE STUDENT_ADDRESS IN ('강원%', '경기%')
+--AND   STUDENT_NO LIKE '9%'
+WHERE STUDENT_ADDRESS LIKE '강원%'
+OR    STUDENT_ADDRESS LIKE '경기%'
+ORDER BY STUDENT_NAME ASC;
+-- 왜... 아무것도 안 뜨는 걸까 
+--* IN 에서는 %를 못 쓰고, LIKE 절에서만 %를 사용할 수 있다
+
+SELECT STUDENT_NO
+FROM TB_STUDENT;
+
+SELECT STUDENT_ADDRESS
+FROM TB_STUDENT;
+
+--------------------------------
+
+-- 3-4 현재 법학과 교수 이름, 주민번호를 나이가 많은 순서로 조회
+SELECT P.PROFESSOR_NAME "교수 이름", P.PROFESSOR_SSN "주민 번호"
+FROM TB_PROFESSOR P
+JOIN TB_CLASS_PROFESSOR CP ON (P.PROFESSOR_NO = CP.PROFESSOR_NO)
+LEFT JOIN TB_CLASS C USING (CLASS_NO)
+WHERE CLASS_NAME LIKE '법학%';
+--ORDER BY PROFESSOR_SSN DESC;
+-- USING 에는 약어 사용 불가
+
+SELECT PROFESSOR_NAME, PROFESSOR_SSN
+FROM TB_PROFESSOR
+WHERE TO_DATE('PROFESSOR_SSN', 'RRMMDD');
+
+
+-----------------------------------
+
+-- 3-5 
+-- 2004 2학기에 'C3118100' 과목 수강한 학생들 학점 조회
+-- 학점 높은 학생부터 표시
+-- 학점이 같으면 학번이 낮은 학생부터 조회
+-- 소수점 아래 2자리까지 0으로 표현 TO_CHAR(NUMBER, 'FM9.00')
+-- 조회결과 양쪽 공백 제거
+SELECT STUDENT_NO, TO_CHAR(POINT, 'FM9.00')
+FROM TB_STUDENT
+JOIN TB_GRADE USING (STUDENT_NO)
+WHERE CLASS_NO = 'C3118100';
+--ORDER BY 2 DESC;
+
+-- FM : 양쪽 공백 제거
+-- 9.00 은  9 오른쪽정렬 0은 왼쪽 정렬하여 빈칸에 숫자를 채운다는 뜻으로 포맷을 지정
+
